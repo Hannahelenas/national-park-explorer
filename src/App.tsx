2f01efc11018757b618603adccb136dfd6822e31
Hanna <hanna@prevetfamily.com>
2025-04-22 15:01:03 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to Park Explorer!</h1>
+      <p>
+        Discover all of America's national parks — your ultimate guide in one
+        place.
+      </p>
+    </>
+  );
+}
+
+export default App;
