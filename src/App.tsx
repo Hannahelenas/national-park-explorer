a9aad567bd89535e39212c064284013c0a28d079
Hanna <hanna@prevetfamily.com>
2025-04-22 16:49:14 +0200

    Install tailwind css and import google fonts

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Welcome to Park Explorer!</h1>
-      <p>
+      <h1 className="text-7xl font-bold text-center text-teal-700">
+        Welcome to Park Explorer!
+      </h1>
+      <p className="text-3xl text-center mt-10">
         Discover all of America's national parks — your ultimate guide in one
         place.
       </p>
