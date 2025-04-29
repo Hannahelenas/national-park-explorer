7eb85d63d1d38712e15118dd38ecf81fd5b68773
Hanna <hanna@prevetfamily.com>
2025-04-29 16:42:23 +0200

    Fetch data from nps api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import HomeHero from "../components/HomeHero";
+import RecentNewsSection from "../components/RecentNewsSection";
 
 export default function Home() {
   return (
     <>
       <HomeHero />
+      <RecentNewsSection />
     </>
   );
 }
