39a6739d17c88e4b8f9b03acb3602c819f1f5c63
Hanna <hanna@prevetfamily.com>
2025-05-14 21:56:01 +0200

    Create Park class

---
+++ src/models/Park.ts
@@ -0,0 +1,46 @@
+export class Park {
+  id: string;
+  fullName: string;
+  parkCode: string;
+  description: string;
+  //Create intercae for activities, topics -TODO
+  states: string;
+  // create interfaace for contacts -TODO
+  directionsInfo: string;
+  images: ParkImage[];
+  weatherInfo: string;
+  name: string;
+  designation: string;
+  // create interface for contacts address: Address[] - TODO
+  constructor(
+    id: string,
+    fullName: string,
+    parkCode: string,
+    description: string,
+    states: string,
+    directionsInfo: string,
+    images: ParkImage[],
+    weatherInfo: string,
+    name: string,
+    designation: string
+  ) {
+    this.id = id;
+    this.fullName = fullName;
+    this.parkCode = parkCode;
+    this.description = description;
+    this.states = states;
+    this.directionsInfo = directionsInfo;
+    this.images = images;
+    this.weatherInfo = weatherInfo;
+    this.name = name;
+    this.designation = designation;
+  }
+}
+
+export interface ParkImage {
+  credit: string;
+  title: string;
+  altText: string;
+  caption: string;
+  url: string;
+}
